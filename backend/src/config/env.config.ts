b1618fe132e0b6438df0b370462f76c4000c261a
Nahom Gebre <nahomgebre73@gmail.com>
2025-10-25 09:42:45 +0300

    port

--- backend/src/config/env.config.ts
+++ backend/src/config/env.config.ts
@@ -2,7 +2,7 @@ import { getEnv } from "../utils/get-env";
 
 export const Env = {
   NODE_ENV: getEnv("NODE_ENV", "development"),
-  PORT: getEnv("PORT"),
+  PORT: process.env.PORT || "8000", // use Render's port if available
   MONGO_URI: getEnv("MONGO_URI"),
   JWT_SECRET: getEnv("JWT_SECRET", "secret_jwt"),
   JWT_EXPIRES_IN: getEnv("JWT_EXPIRES_IN", "15m"),
